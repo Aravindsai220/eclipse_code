4f4938a396a5c737f206aa55e9b2e5d71ef34b2b
USER <USER@DESKTOP-VGOQU57.lan>
2025-11-20 21:29:08 +0530

    first commit

---
+++ src/com/corejava/Main.java
@@ -0,0 +1,17 @@
+package com.corejava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b=126;
+		System.out.println(b);
+		int  i= 22;
+		System.out.println(i);
+		for (  i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
